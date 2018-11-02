78abbf768be5a99342c1cb95c53920e01501c60a
Oscar Chan <chanoscar0@gmail.com>
2018-11-02 10:48:27 -0700

    PREWEBPACKFIX

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, '/dist'),
     filename: 'webpack-bundle.js',
-    publicPath: '/dist/',
+    publicPath: '../dist/',
   },
   // Compile for Electron for main process.
   target: 'electron-main',
