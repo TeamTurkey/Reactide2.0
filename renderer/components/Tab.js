891ec8b710dc1906ea406a0d289b8331736fada3
Oscar Chan <chanoscar0@gmail.com>
2018-11-06 20:58:06 -0800

    import Path base functionality, no edge-cases

--- renderer/components/Tab.js
+++ renderer/components/Tab.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import Box from 'box';
 const Tab = ({ name, setActiveTab, path, closeTab }) => {
   return (
     <li className="texteditor tab" onClick={() => { setActiveTab(path); }} >
       <div className="title">{name}</div>
+      <Box onClick = {setActiveBox} color='red' id={2}></Box>
+      <Row color='blue'></Row>
       <div className="close-icon" onClick={(event) => { closeTab(path, event); }} />
     </li>
   );
