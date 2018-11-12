1e03aa235f019aa74fcd056dd438e1a4289a3532
Oscar Chan <chanoscar0@gmail.com>
2018-11-12 13:49:21 -0800

    commit

--- renderer/components/Terminal.js
+++ renderer/components/Terminal.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import { Terminal } from 'xterm'
+import { Terminal } from 'xterm';
+import shell from 'shelljs';
 import * as fit from 'xterm/lib/addons/fit/fit';
 Terminal.applyAddon(fit);
-
 class XTerm extends React.Component {
     constructor() {
       super();
       this.state = {
         currCommand: '',
-        cwd: 'something',
-        user: 'oscarchan',
+        cwd: '',
       }
     }
     componentDidMount(){
       //Declaring actual terminal
+      
+
       console.log("Creating new terminal");
       const term = new Terminal();
       term.setOption('cursorStyle', 'block');
@@ -22,15 +23,12 @@ class XTerm extends React.Component {
       term.open(document.getElementById('terminal'));
       let greeting = this.state.user + ' ' + this.state.cwd + '\r\n';
       term.write(greeting);
-      term.write('$')
       term.fit();
       term.on('key', (key,ev) => {
-        console.log('This is the ev object', ev);
         if(ev.keyCode === 13) {
           term.write('\r\n' + greeting);
-          term.write('$');
-          term.write('\x1b[C');
-          console.log('hello');
+          term.write('$ ');
+          term.write("\u001B[1C")
           //run something in the terminal
           //output the response and then reset currCommand
           this.setState({currCommand: ''});
@@ -38,10 +36,6 @@ class XTerm extends React.Component {
         if(ev.keyCode === 8) {
           term.write('\b \b');
           this.setState({currCommand: this.state.currCommand.slice(0, -1)});
-          // term.selectAll();
-          // console.log('CURRENT SELECTION', typeof term.getSelection());
-          // console.log('Clearing contents');
-          // term.clear();
         } else{
           term.write(key);
           this.setState({currCommand: this.state.currCommand + key});
@@ -51,13 +45,14 @@ class XTerm extends React.Component {
       // xterm.on('data', (data) => {
       //   ptyProcess.write(data);
       // });
+      xterm.on('data', (data) => {
+        ptyProcess.write(data);
+      });
     }
     render() {
-      return (
-        <div>
+      return ( 
           <div id='terminal'>
           </div>
-        </div>
       )
       
     }
