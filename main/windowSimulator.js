3e4456eda5594cca15d50fa1be0adfaa0f7e7f84
Pablo Lee <pablolee@Pablos-MacBook-Pro.local>
2018-11-20 09:18:07 -0800

    add correct link path for create react app

--- main/windowSimulator.js
+++ main/windowSimulator.js
@@ -9,27 +9,17 @@ const {spawn} = require('child_process');
 const windowSimulator = () => {
 
   const projInfo = JSON.parse(fs.readFileSync(path.join(__dirname, '../lib/projInfo.js')));
-  console.log(projInfo);
-  console.log('MESSAGE RECEIVED')
+
   //Simulation for CRA
   if (projInfo.devServerScript === 'start') {
-    console.log('RUNNING NPM START');
     const child = spawn('npm', ['start'], {cwd: projInfo.rootPath});
       child.stdout.on('data', (data) => {
         global.mainWindow.webContents.send('start simulator',['http://localhost:3000',child.pid]);
       });
-      child.on('close', (data) => {
-        console.log('ONCLOSE PID', child.pid);
-      })
-      // (err, stdout, stderr) => {
-      //   if(err) console.log(err);
-      //   console.log('ABOUT TO SEND PAYLOAD')
-      // }
   //Simulation for react-dev-server
   } else if (projInfo.devServerScript === 'run dev-server') {
     let child = spawn('npm',  ['run', 'dev-server'], {cwd: projInfo.rootPath});
       child.stdout.on('data', (data) => {
-        console.log("About to run simluttioe");
         global.mainWindow.webContents.send('start simulator',['http://localhost:8085', child.pid]);
     })
   } else if (projInfo.htmlPath) {
